ee8a433015a763b295e46527e9b655d6e42af015
Olga Krukova <krukovaolga1@gmail.com>
2024-07-14 08:11:28 +0300

    feat: install jest libraries

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-export const testEnvironment = 'jsdom';
-export const setupFilesAfterEnv = ['<rootDir>/src/setupTests.js'];
-export const moduleNameMapper = {
-  '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-  '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
-};
-export const collectCoverage = true;
-export const coverageReporters = ['text', 'lcov'];
-export const coverageDirectory = 'coverage';
